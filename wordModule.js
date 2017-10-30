9601c20c7cf8c322c68008da24d9d0d8e9ef3b82
dailybp <bpdailey@gmail.com>
2017-10-30 18:17:33 -0400

    Second Draft

--- wordModule.js
+++ wordModule.js
@@ -4,15 +4,26 @@ function word(str){
   this.word = wrd;
   this.letter = [];
   this.wordFound = false;
+
+
   this.getLetters = function(){
     for(var i = 0; i < this.word.length; i++){
       var newLetter = new Letter (this.word[i]);
       this.letter.push(newLetter);
     }
   };
-  this.wordFound = function(){
-    if(this.letters.every(function(letter)){
-      return
-    })
+
+
+  this.wasWordFound = function(){
+    if(this.letters.every(function(letter){
+      return letter.appear === true;
+    })){
+      this.wordFound = true;
+      return true;
+    }
+  };
+
+  this.wasLetterFound = function(guessedLetter){
+    var outputTo
   }
 }
